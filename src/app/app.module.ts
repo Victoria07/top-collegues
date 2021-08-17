a281ebec65af75aad3d2b443aaabfad9851e6a1c
Victoria <vic.iber16@gmail.com>
2021-08-17 11:34:31 +0200

    Mise à jour. TP formulaire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {MatCardModule} from '@angular/material/card';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {AppComponent} from './app.component';
+import {CardComponent} from "./components/card/card.component";
+import {CollegueComponent} from "./components/collegue/collegue.component";
+import {AvisComponent} from "./components/avis/avis.component";
+import {FormulaireComponent} from './components/formulaire/formulaire.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from '@angular/material/input';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    AvisComponent,
+    CollegueComponent,
+    FormulaireComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
